13083c9eab45f71aa04092921140a1855ba1588c
Welcome <Welcome@DESKTOP-H7IFQSF.bbrouter>
2023-06-15 23:06:05 +0530

    second test

--- Firstmavenproject/src/main/java/com/auto/scripts/BlueStoneTest.java
+++ Firstmavenproject/src/main/java/com/auto/scripts/BlueStoneTest.java
@@ -16,27 +16,64 @@ public class BlueStoneTest {
 	
 	@BeforeMethod
 	
-	public void FirstTest()  {
+	public void FirstTest() throws InterruptedException  {
 		WebDriverManager.chromedriver().setup();
 		Driver = new ChromeDriver();
 		
 		Driver.get("https://www.bluestone.com");
 		Driver.manage().window().maximize();
+		Thread.sleep(3000);
 		
 		Driver.findElement(By.xpath("//*[@id='denyBtn']")).click();
 		Driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 	}
 	
-	@Test
+	@Test(priority = 1)
 	
-	public void Test1() {
+	public void Test1() throws InterruptedException {
 		BlueStonepage BP = new BlueStonepage(Driver);
 		BP.HoverWatch();
+		Thread.sleep(3000);
 		BP.ClickCharm();
+		Thread.sleep(3000);
 		BP.HoverPrice();
+		Thread.sleep(3000);
 		BP.ClickBelow();
+		Thread.sleep(3000);
+		BP.ClickRemoveRF();
+		Thread.sleep(3000);
 	}
-	@Test
+	@Test(priority = 2)
+	public void Test2() throws InterruptedException {
+		BlueStonepage BP = new BlueStonepage(Driver);
+		BP.HoverWatch();
+		Thread.sleep(3000);
+		BP.ClickBand();
+		Thread.sleep(3000);
+		BP.HoverDelivery();
+		Thread.sleep(3000);
+		BP.ClickDeliver();
+		Thread.sleep(3000);
+		BP.ClickRemoveNF();
+		Thread.sleep(3000);
+	}
+	@Test(priority = 3)
+	public void Test3() throws InterruptedException {
+		BlueStonepage BP = new BlueStonepage(Driver);
+		Thread.sleep(3000);
+		BP.HoverWatch();
+		Thread.sleep(3000);
+		BP.ClickPin();
+		Thread.sleep(3000);
+		BP.HoverOffer();
+		Thread.sleep(3000);
+		BP.ClickMaking();
+		Thread.sleep(3000);
+		BP.ClickRemoveOF();
+		Thread.sleep(3000);
+	}
+	
+	
 	
 	
 	@AfterMethod
--- Firstmavenproject/src/main/java/com/auto/scripts/BlueStonepage.java
+++ Firstmavenproject/src/main/java/com/auto/scripts/BlueStonepage.java
@@ -20,11 +20,24 @@ public BlueStonepage(WebDriver d){
  	@FindBy (xpath = ("//a[text()='Charm']")) WebElement Charm;
  	@FindBy (xpath = ("//span[text()='Price']")) WebElement Price;
  	@FindBy(xpath = ("//span[@class='prcs-dlh']")) WebElement Below;
+ 	@FindBy(xpath = ("//span[@class='WebRupee']")) WebElement RemoveRf;	
+ 	@FindBy(xpath = ("//a[@title='Band']")) WebElement Band;
+ 	@FindBy(xpath = ("//section[@id='Delivery Time-form']/span/span")) WebElement Delivery;
+ 	@FindBy(xpath = ("//span[@data-tagcategory='Delivery Time']']")) WebElement Deliverytime;
+ 	@FindBy(xpath = ("//span[text()='Next Day Delivery']")) WebElement RemoveNF;	
+ 	@FindBy(xpath = ("//a[@title='Pin']")) WebElement Pin;
+ 	@FindBy(xpath = ("//span[text()='Offers']")) WebElement Offers;
+ 	@FindBy(xpath = ("//span[@data-displayname='10% Making Charge Off']")) WebElement makingcharge;
+ 	@FindBy(xpath = ("//span[text()='10% Making Charge Off']")) WebElement RemoveOF;
+// 	@FindBy(xpath = ("//span[@class='prcs-dlh']")) WebElement Below;
  	
+ 	//Test-case 1
  	public void HoverWatch() {
  		Actions A = new Actions(dr);
  		A.moveToElement(WatchJewe).perform();
  	}
+ 	
+	//Test-case 1.1
  	public void ClickCharm() {
  		Charm.click();
  	}
@@ -35,11 +48,44 @@ public BlueStonepage(WebDriver d){
  	public void ClickBelow() {
  		Below.click();
  	}
+ 	public void ClickRemoveRF() {
+ 		RemoveRf.click();
+ 	}
+ 	
+	//Test-case 1.2
+ 	public void ClickBand() {
+ 		Band.click();
+ 	}
+ 	
+ 	public void HoverDelivery() {
+ 		Actions A3 = new Actions(dr);
+ 		A3.moveToElement(Delivery).perform();
+ 	}
+ 	public void ClickDeliver() {
+ 		Deliverytime.click();
+ 	}
 
+ 	public void ClickRemoveNF() {
+ 		RemoveNF.click();
+ 	}
 
-
-
-
-
-
+ 	//Test-case 1.3
+ 	public void ClickPin() {
+ 		Pin.click();
+ 	}
+ 	public void HoverOffer() {
+ 		Actions A4 = new Actions(dr);
+ 		A4.moveToElement(Offers).perform();
+ 	}
+ 	
+ 	public void ClickMaking() {
+ 		makingcharge.click();
+ 	}
+ 	
+ 	public void ClickRemoveOF() {
+ 		RemoveOF.click();
+ 	}
+ 	
+ 	
+ 	
 }
